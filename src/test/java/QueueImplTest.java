698f8e894be1d99096aa40a7b049f866a8d7f429
mrezlik <mrezlik@gmail.com>
2018-05-15 18:37:27 +0200

    Add simple test of creation Queue instance

---
+++ src/test/java/QueueImplTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QueueImplTest {
+
+    @Test
+    void testQueueCreation(){
+        Queue queue = new QueueImpl();
+    }
+
+}
